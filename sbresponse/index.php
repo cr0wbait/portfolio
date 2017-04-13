86ac1e338aa79c06769175bd5094c2d5169314f2
iambateman <stephen.bateman@truematter.com>
2017-04-13 15:05:13 -0400

    adding a response

---
+++ sbresponse/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$title = "TITLEZ!";
+
+ ?>
+
+<?php include 'includes/header.php'; ?>
+
+
+
+We're using PHP.
+
+:) 
+
+It's dirty and gross.
+
+By the way, I'm on headphones so feel free to talk.
+
+
+<?php include 'includes/footer.php'; ?>
